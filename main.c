5217556942a9b20102a0c96d949551d09e038cc4
Jannik Glane <jannikglane@gmail.com>
2022-03-29 12:51:42 +0200

    feat: Eingabevalidierung bei Tag und Monat

--- main.c
+++ main.c
@@ -26,23 +26,33 @@ int main()
     printf("Bitte das Jahr angeben: ");
     scanf("%i", &jahr);
 
-    // Eingabe des Monats
-    printf("Bitte den Monat angeben: ");
-    scanf("%i", &monat);
-
-    // Eingabe des Tages
-    printf("Bitte den Tag angeben: "); // TODO check ob tag im monat ist
-    scanf("%i", &tag);
+    do
+    {
+        // Eingabe des Monats
+        printf("Bitte den Monat angeben: ");
+        scanf("%i", &monat);
+    }
+    while(monat < 1 || monat > sizeof(tage_pro_monat)/sizeof(tage_pro_monat[0]));
 
     // Aufruf einer Funktion die bestimmt ob das übergebene Jahr ein Schaltjahr ist
     if(IstSchaltjahr(jahr))
         tage_pro_monat[1] = 29;
+
+    do
+    {
+        // Eingabe des Tages
+        printf("Bitte den Tag angeben: ");
+        scanf("%i", &tag);
+    }
+    while(tag < 1 || tag > tage_pro_monat[monat-1]);
 
     // Aufruf einer Funktion die die Tage der Monate zusammenrechnet
     tageGesamt = TagErmitteln(tag, monat, tage_pro_monat);
 
     // Ausgabe der Summer der Tage
     printf("Heute ist der %i. Tag im Jahr", tageGesamt);
+
+    return 0;
 }
 
 int IstSchaltjahr(int jahr)
